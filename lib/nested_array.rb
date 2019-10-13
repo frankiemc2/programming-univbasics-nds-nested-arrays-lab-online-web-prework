3238fe97fc28c5b22293d372ece517161fd80510
francisco cabrera <fjcabrera@buffalo.edu>
2019-10-13 20:25:59 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -18,21 +18,17 @@ ENFIELD_TENNIS_ACADEMY = [
 ]
 
 def assembled_matrix
-  ENNET_HOUSE: = [
-    "Don Gately",
+  assembled_matrix =
+  [ "Don Gately",
     "Joelle van Dyne",
     "Pat M.",
     "Kate Gompert",
-    "Bruce Green"
-  ]
-
-  ENFIELD_TENNIS_ACADEMY: = [
-    "Hal Incandenza",
+    "Bruce Green"  ]
+  [ "Hal Incandenza",
     "Lyle",
     "Gerhard Schtitt",
     "Mario Incandenza",
-    "Michael Pemulis"
-  ]
+    "Michael Pemulis" ]
 end
 
 def array_literal_matrix
